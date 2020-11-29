efda15814bf851deb4e1b4a21d0d04716fa41307
Mary Jimenez <maryjios>
2020-11-29 12:38:30 -0500

    Agregando clase App al index

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
-<?php 
+<?php
+require_once 'libs/database.php';
+require_once 'libs/controller.php';
+require_once 'libs/view.php';
+require_once 'libs/model.php';
+require_once 'libs/app.php';
 
-    header("Location: http://localhost/AgroPlaza%20Demo/views/main/index.php");
- ?>
+require_once 'config/config.php';
+
+$app = new App();
+
+?>
